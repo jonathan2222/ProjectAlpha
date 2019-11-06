6b8659e5d3e9039047bc984f8ad62382bf185b42
Adrian <adde--97@hotmail.com>
2019-11-06 16:23:07 +0100

    Moved code to renderer

--- Projects/Game/src/Main.cpp
+++ Projects/Game/src/Main.cpp
@@ -65,34 +65,7 @@ int main(int argc, char* argv[])
 		cam.freeMove(timer.getDeltaTime());
 		display.getWindow().setView(cam.getView());
 
-		// PUT THIS IN NEW CLASS
-		sf::Vector2f camPos = cam.getCentre();
-		if (abs(cam.getCentre().x) > CHUNK_SIZE * CELL_SIZE) {
-			cam.setCentre(sf::Vector2f(0.f, camPos.y));
-			gridPos.x += int(abs(camPos.x) / camPos.x);
-
-			std::vector<pa::Chunk*> chunks = grid.getChunks();
-			for (int x = 0; x < gridCols; x++) {
-				for (int y = 0; y < gridRows; y++) {
-					pa::Chunk * c = chunks[x + y * gridCols];
-					wb.generate(gridPos.x + x, gridPos.y + y, c);
-					grid.updateUVCoords(sf::Vector2u(x, y));
-				}
-			}
-		}
-		if (abs(cam.getCentre().y) > CHUNK_SIZE * CELL_SIZE) {
-			cam.setCentre(sf::Vector2f(camPos.x, 0.f));
-			gridPos.y += int(abs(camPos.y) / camPos.y);
-
-			std::vector<pa::Chunk*> chunks = grid.getChunks();
-			for (int x = 0; x < gridCols; x++) {
-				for (int y = 0; y < gridRows; y++) {
-					pa::Chunk * c = chunks[x + y * gridCols];
-					wb.generate(gridPos.x + x, gridPos.y + y, c);
-					grid.updateUVCoords(sf::Vector2u(x, y));
-				}
-			}
-		}
+		
 			
 		// Draw chunks
 		grid.draw(display.getWindow(), states);
